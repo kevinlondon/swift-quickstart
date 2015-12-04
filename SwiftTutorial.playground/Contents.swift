c2949d1489b3343bf1084cb5f6fabd58b06f8922
kevinlondon <kevinlondon@gmail.com>
2015-12-04 13:06:52 -0800

    Initial commit, end of first tutorial.

---
+++ SwiftTutorial.playground/Contents.swift
@@ -0,0 +1,28 @@
+class TipCalculator {
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
+
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.09)
+print(String(tipCalc.returnPossibleTips()))
